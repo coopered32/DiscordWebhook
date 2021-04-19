e21c4984a1c80bf83cc6591c916a034ffc39e2bb
coopdesktop <coopdesktop@COOPDESKTOP>
2021-04-18 22:26:38 -0600

    Updated documentation

--- DiscordWebhook/DiscordWebhook.cs
+++ DiscordWebhook/DiscordWebhook.cs
@@ -8,17 +8,17 @@ namespace Discord.Webhook
     public class DiscordWebhook
     {
         /// <summary>
-        /// Url that can be used for all future webhook requests
+        /// Url that can be used for all webhook requests
         /// </summary>
         public string Url { get; set; }
 
         /// <summary>
-        /// User that can be used for all future webhook requests
+        /// Username that will be associated with the message
         /// </summary>
         public string User { get; set; }
 
         /// <summary>
-        /// Sends discord message using webhook url
+        /// Sends discord message using webhook url and username
         /// </summary>
         /// <param name="message">Message to be sent</param>
         public void SendMessage(string message)
