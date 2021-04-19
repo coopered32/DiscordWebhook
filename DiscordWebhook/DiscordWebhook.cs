58ce3759d620af2910b4d226ff19b9b2516fc7d1
coopdesktop <coopdesktop@COOPDESKTOP>
2021-04-18 22:22:24 -0600

    Add project files.

---
+++ DiscordWebhook/DiscordWebhook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Discord.Webhook
+{
+    public class DiscordWebhook
+    {
+        /// <summary>
+        /// Url that can be used for all future webhook requests
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// User that can be used for all future webhook requests
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Sends discord message using webhook url
+        /// </summary>
+        /// <param name="message">Message to be sent</param>
+        public void SendMessage(string message)
+        {
+            WebClient wc = new();
+
+            try
+            {
+                wc.UploadValues(this.Url, new System.Collections.Specialized.NameValueCollection {
+                    {
+                        "content", message
+                    },
+                    {
+                        "username", this.User
+                    }
+                });
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
